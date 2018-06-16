fcca5bcb02be384a8ff4bc66957d5e389e07ff8c
Jenny-Le <lejenny75@gmail.com>
2018-06-16 13:48:43 -0700

    Home Page rendered

--- fanpage/src/components/HomePage.js
+++ fanpage/src/components/HomePage.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-
 const HomePage = () => {
-
-}
-
+    return(
+          <div className="container-fluid">
+          <h1>Odesza</h1>
+          
+          
+          
+          </div>
+    )
+      }
 
 export default HomePage;
---
+++ fanpage/src/components/SplashPage.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+const SplashPage = () => {
+    
+   return(
+    <a href="/HomePage">
+    <img src="https://assets.privy.com/picture_photos/111568/medium/00fa8a5366b14da19f422a0bcc3f1af4?1492999817" />
+  </a>
+   )
+}
+
+
+
+
+
+
+export default SplashPage;
